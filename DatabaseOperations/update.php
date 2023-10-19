79e33edc62fa24934202497bb69248e735f2dc10
SnehaLatha-D <143076019+SnehaLatha-D@users.noreply.github.com>
2023-10-19 15:24:25 +0530

    Add files via upload

--- DatabaseOperations/update.php
+++ DatabaseOperations/update.php
@@ -1,9 +1,17 @@
 <?php include 'header.php'; ?>
 <div id="main-content" align="center">
-<h2>Update Record</h2>
+<h2>Edit Record</h2>
+<form class="post-form" action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+<div class="form-group">
+<label>Id</label>
+<input type="text" name="sid" />
+</div>
+<input class="submit" type="submit" name="showbtn" value="Show" />
+</form>
 <?php
+if(isset($_POST['showbtn'])){
 include 'config.php';
-$stu_id = $_GET['id'];
+$stu_id = $_POST['sid'];
 $sql = "SELECT * FROM student WHERE sid = {$stu_id}";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 if(mysqli_num_rows($result) > 0) {
@@ -11,13 +19,13 @@ while($row = mysqli_fetch_assoc($result)){
 ?>
 <form class="post-form" action="updatedata.php" method="post">
 <div class="form-group">
-<label>Name</label>
-<input type="hidden" name="sid" value="<?php echo $row['sid']; ?>"/>
-<input type="text" name="sname" value="<?php echo $row['sname']; ?>"/>
+<label for="">Name</label>
+<input type="hidden" name="sid" value="<?php echo $row['sid']; ?>" />
+<input type="text" name="sname" value="<?php echo $row['sname']; ?>" />
 </div>
 <div class="form-group">
 <label>Address</label>
-<input type="text" name="saddress" value="<?php echo $row['saddress']; ?>"/>
+<input type="text" name="saddress" value="<?php echo $row['saddress']; ?>" />
 </div>
 <div class="form-group">
 <label>Class</label>
@@ -40,11 +48,12 @@ echo "</select>";
 </div>
 <div class="form-group">
 <label>Phone</label>
-<input type="text" name="sphone" value="<?php echo $row['sphone']; ?>"/>
+<input type="text" name="sphone" value="<?php echo $row['sphone']; ?>" />
 </div>
-<input class="submit" type="submit" value="Update"/>
+<input class="submit" type="submit" value="Update" />
 </form>
 <?php
+}
 }
 }
 ?>
@@ -65,6 +74,6 @@ text-align:center;
 </style>
 </head>
 <div class="footer">
-<p align="center"> &copy; N.Ushasree--Allrights Reserved
+<p align="center"> &copy; M.Hemanth Kumar Rao--Allrights Reserved
 </body>
 </html>
